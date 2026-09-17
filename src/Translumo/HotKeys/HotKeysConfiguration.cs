16c560df34006007b19504f0b1f71e4c23796d05
Jose Bustillo <josebustillodealba@gmail.com>
2026-09-17 13:24:38 -0500

    fix(hotkeys): restore alt+f as the default once-translate hotkey

--- src/Translumo/HotKeys/HotKeysConfiguration.cs
+++ src/Translumo/HotKeys/HotKeysConfiguration.cs
@@ -13,7 +13,7 @@ namespace Translumo.HotKeys
             SettingVisibilityKey = new HotKeyInfo(Key.G, KeyModifier.Alt),
             TranslationStateKey = new HotKeyInfo(Key.OemTilde, KeyModifier.None),
             ShowSelectionAreaKey = new HotKeyInfo(Key.Y, KeyModifier.Alt),
-            OnceTranslateKey = new HotKeyInfo(Key.F, KeyModifier.Ctrl | KeyModifier.Shift),
+            OnceTranslateKey = new HotKeyInfo(Key.F, KeyModifier.Alt),
             WindowStyleChangeKey = new HotKeyInfo(Key.T, KeyModifier.Ctrl),
 
             ChatVisibilityGamepadKey = new GamepadHotKeyInfo(GamepadKeyCode.None),
